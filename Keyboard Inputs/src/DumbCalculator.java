c33bda9fb10ea1f9f209a21f7f4441964161969d
kekeana <OldhamK7536@sduhsd.lan>
2017-12-04 11:12:19 -0800

    lab 3

---
+++ Keyboard Inputs/src/DumbCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DumbCalculator 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		double num1, num2, num3, answer;
+		
+		System.out.println("Let's find the average of a few numbers... ");
+		
+		System.out.println("");
+		
+		System.out.println("What is the first number?");
+		num1 = keyboard.nextDouble();
+		
+		System.out.println("What is the second number?");
+		num2 = keyboard.nextDouble();
+
+	}
+
+}
